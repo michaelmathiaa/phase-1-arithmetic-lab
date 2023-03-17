011a9e78bb0b5fa48174d0e2f5d5760a7b1fa8ba
Michael Mathias <michaelscodingjourney@gmail.com>
2023-03-17 12:33:42 -0700

    Passed four tests for arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+ 
+const random = Number.isInteger(random) + 1;
+
+const num3 = 15;
+const num4 = 11;
+const mod = num3 % num4;
+
+const max = Math.max(1, 13, 8, 4, 10, 15, 2, 5, 20, 7, 3);
